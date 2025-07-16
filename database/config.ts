5c225f4b82bb1b69f939bc5591a7a037463e8a78
Agustin Mancini <agustinmancini2@hotmail.com>
2025-07-16 00:14:38 -0300

    Update config.ts

--- database/config.ts
+++ database/config.ts
@@ -1,3 +1,4 @@
+/*
 import mongoose from "mongoose";
 
 export const dbConnection = async (): Promise<void> =>{
@@ -8,6 +9,31 @@ export const dbConnection = async (): Promise<void> =>{
         }
 
         await mongoose.connect(dbURL)
+        
+    } catch (error){
+        console.log(error);
+        throw new Error('Ocurrió un error al iniciar la base de datos. Por favor, intentelo nuevamente.')
+    }
+}
+*/
+
+import mongoose from "mongoose";
+
+export const dbConnection = async (): Promise<void> =>{
+    try{
+        const dbURL = process.env.DB_URL;
+        if(!dbURL){
+            throw new Error('La URL no fue definida correctamente dentro del archivo .env')
+        }
+
+        await mongoose.connect(process.env.MONGO_URI || '', {
+  dbName: 'spaceshipDB'
+})
+.then(() => console.log('✅ MongoDB conectado'))
+.catch(err => {
+  console.error('❌ Error al conectar a MongoDB:', err.message);
+});
+
         
     } catch (error){
         console.log(error);
